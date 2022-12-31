587894e35d5f08b1ca9d217d450b13a4160b17f7
brigantinojoe <103912190+brigantinojoe@users.noreply.github.com>
2022-12-30 16:53:54 -0800

    Finish routes in /routes/api/product-routes.js. Completed routes include: get all, get one by id, and delete/destroy.

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const ProductTag = require('./ProductTag');
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE'
+  onDelete: "SET NULL"
 });
 // Products belongsTo Category
 Product.belongsTo(Category, {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,31 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }]
+    });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }]
+    });
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // create new product
@@ -89,8 +105,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const product = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(`Product ID ${req.params.id} has been deleted.`);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
